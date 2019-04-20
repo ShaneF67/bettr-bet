750bf400a618aa3e8983f30e89828b6c7543e346
ShaneF67 <shanefeehily9@hotmail.com>
2019-04-20 09:37:47 -0500

    initial-ommit

---
+++ src/main/java/com/bettr/bettrbet/BettrBetApplication.java
@@ -0,0 +1,13 @@
+package com.bettr.bettrbet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BettrBetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BettrBetApplication.class, args);
+	}
+
+}
